5299bee29041aed1630c9a6d19e4abba07a1cb3a
seothemes <seothemeswp@gmail.com>
2022-11-11 21:17:12 +0800

    Init

---
+++ patterns/feature-solutions.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Title: Feature Solutions
+ * Slug: feature-solutions
+ * Categories: feature
+ * 
+ */
+?>
+<!-- wp:spacer {"height":"2em","style":{"\u002d\u002ddisplay":"none","\u002d\u002ddisplay-desktop":"flex"}} -->
+<div style="height:2em" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","right":"0","bottom":"0","left":"0"},"margin":{"top":"0"}}},"className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;padding-top:var(--wp--preset--spacing--md);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns {"verticalAlignment":"bottom","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom","style":{"\u002d\u002dposition":"relative"}} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:image {"width":45,"height":45,"style":{"svgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 xml:space=\u0022preserve\u0022 viewBox=\u00220 0 154.7 217\u0022 fill=\u0022currentColor\u0022 width=\u002240\u0022\u003e   \u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022m150 170-1.7 1c.4-2.2-3.3-5.6-4.9-3.3-.7-2-2.6-3-4.6-2.5-22.5 2.3-57.9 14.2-86.2 18.3-10.3 4.6-35.3-2.7-37.3 12.8 20.5 24.1 77.4-2.7 105.1-8.3 9.9-4.5 25-5.3 30.3-13.5 3 .1-1-3.5-.7-4.5zM6.2 94.1c-.4 5.2 12.4 18.7 19.9 15-1.9 2.4 16.6 7.9 17.4 3.3 2 3.3 20.5 7.4 23.2 3.2 4.8 5.1 23.3 6.3 26.3 5.5-2.9 1.4 6.6 8.4 7.1 4.9 2.5.5 4.7 3.1 4.8-1.1 1.9 4.2 4.4 2.1 6.5-.2-12.2-21.2-51.1-22.4-73.1-24.8-5.1-1-7.4-2.6-14.4-2.4C21 96.1 8 88.1 6.2 94.1m36.7-80.4c15.2 19.8 38.2 44.7 60.8 51.1 30.4 10.1-64.9-77.3-60.8-51.1z\u0022 clip-rule=\u0022evenodd\u0022\u003e \u003c/path\u003e\u003c/svg\u003e","\u002d\u002dposition":"absolute","\u002d\u002dinset":"auto auto auto -2em"},"className":"is-style-svg"} -->
+<figure class="wp-block-image is-resized is-style-svg"><img src="#" alt="" width="45" height="45"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<h2 style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">We <span class="has-font-family has-playfair-display-font-family" style=""><em>solve problems</em></span> using software</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:paragraph {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xxs"}}}} -->
+<p style="padding-bottom:var(--wp--preset--spacing--xxs)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:heading {"level":4} -->
+<h4>Search engine optimization</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|lg"}}}} -->
+<h4 style="margin-top:var(--wp--preset--spacing--lg)">Conversion rate optimization</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|lg"}},"\u002d\u002dposition":"relative"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--lg);flex-basis:66.66%"><!-- wp:group {"style":{"border":{"radius":"9999px"},"filter":{"backdrop":true},"spacing":{"blockGap":"0"},"color":{"background":"#4447ed","text":"#fffffffc"},"\u002d\u002dposition":"absolute","\u002d\u002dinset":"0px -2em auto auto","\u002d\u002dz-index":1,"\u002d\u002dposition-desktop":"absolute","\u002d\u002dinset-desktop":"10px auto auto -70px","backdrop":{"filter":{}}},"layout":{"type":"flex","flexWrap":"nowrap"},"boxShadow":{"useDefault":false}} -->
+<div class="wp-block-group has-text-color has-background" style="border-radius:9999px;color:#fffffffc;background-color:#4447ed"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","letterSpacing":"0.01em","fontSize":"16px"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"radius":"999px"},"transform":{"rotate":"360","animate":{"rotate":"-360"}},"animation":{"transform":{"duration":"20","iterationCount":"-1"},"duration":"50","delay":"0","iterationCount":"-1","event":"infinite"}},"className":"is-style-curved-text","curvedText":{"content":"Contact Us · Contact Us · Contact Us · ✦ · ","containerSize":"140","pathSize":"94","svgString":"\u003csvg viewBox=\u00220 0 140 140\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022 enable-background=\u0022new 0 0 140 140\u0022 xml:space=\u0022preserve\u0022 width=\u0022140\u0022 height=\u0022140\u0022 contenteditable=\u0022false\u0022 x=\u00220\u0022 y=\u00220\u0022\u003e\u003cpath id=\u0022circle-1668087234676.5352\u0022 d=\u0022M 70, 70 m -47, 0 a 47,47 0 0,1 94,0 a 47,47 0 0,1 -94,0\u0022 fill=\u0022transparent\u0022\u003e \u003c/path\u003e\u003ctext fill=\u0022currentColor\u0022\u003e\u003ctextPath xlink:href=\u0022#circle-1668087234676.5352\u0022\u003eContact Us · Contact Us · Contact Us · ✦ ·  \u003c/textPath\u003e\u003c/text\u003e\u003c/svg\u003e"}} -->
+<p class="is-style-curved-text has-transform has-animation" style="border-radius:999px;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:16px;font-style:normal;font-weight:500;letter-spacing:0.01em;--transform:rotate(360deg);--transform-animate:rotate(-360deg);--animation:animate 50s 0s infinite linear;animation-play-state:paused"><svg viewBox="0 0 140 140" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 140 140" xml:space="preserve" width="140" height="140" contenteditable="false" x="0" y="0"><path id="circle-1668141338999.3926" d="M 70, 70 m -47, 0 a 47,47 0 0,1 94,0 a 47,47 0 0,1 -94,0" fill="transparent"> </path><text fill="currentColor"><textPath xlink:href="#circle-1668141338999.3926">Contact Us · Contact Us · Contact Us · ✦ · </textPath></text></svg></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"linkDestination":"media","style":{"transform":{"rotateX":"1","rotate":"-45","hover":{"scale":"1.1"}},"\u002d\u002dposition":"absolute","\u002d\u002dinset":"0px 0px 0px 0px","\u002d\u002dinset-desktop":"0px 0px 0px 0px","color":{"text":"#fffffffc"}},"className":"is-style-icon","iconName":"arrow-right","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022\u003e\u003cpath d=\u0022m14.3 6.7-1.1 1.1 4 4H4v1.5h13.3l-4.1 4.4 1.1 1.1 5.8-6.3z\u0022/\u003e\u003c/svg\u003e"} -->
+<figure class="wp-block-image is-style-icon has-text-color has-transform" style="color:#fffffffc;--transform:rotate(-45deg) rotateX(1deg);--transform-hover:scale(1.1);--wp--custom--icon--color:#fffffffc;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot;&gt;<path d=&quot;m14.3 6.7-1.1 1.1 4 4H4v1.5h13.3l-4.1 4.4 1.1 1.1 5.8-6.3z&quot;/&gt;</svg&gt;')"><a href="#"><img src="#" alt=""/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"id":497,"sizeSlug":"full","linkDestination":"none","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":""} -->
+<figure class="wp-block-image size-full" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="<?php echo content_url( "/themes/brandify/assets/img/" ) ?>studio-office-man.jpeg" alt="" class="wp-image-497"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ readme.txt
@@ -0,0 +1,66 @@
+=== Brandify ===
+Contributors: blockify
+Requires at least: 6.1
+Tested up to: 6.1
+Stable tag: 0.0.1
+Requires PHP: 7.4
+License: GPL-2.0-or-later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Modern full site editing enabled child theme for Blockify. Features fully customizable animations, custom SVG illustrations, responsive positioning settings and more. Perfect for building a portfolio, blog or business website.
+
+== Installation ==
+
+1. In your site's admin panel, go to Appearance > Themes and click `Add New`.
+2. Type "Brandify" in the search field.
+3. Click `Install` and then `Activate` to start using the theme.
+4. Navigate to Appearance > Editor.
+5. Edit the page content as you wish, you can add, remove or customize any of the blocks.
+6. Enjoy :)
+
+== Copyright ==
+
+© Copyright Blockify 2022. All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details,
+
+Google Fonts
+License: SIL Open Font License, 1.1
+License URL: https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+Feather SVG Icons
+License: MIT
+License URL: https://github.com/feathericons/feather/blob/master/LICENSE
+
+SVG Illustrations
+All inline SVG illustrations and placeholder logos designed by the author and licensed under GNU General Public License, version 2 or later.
+
+All JPG images licensed under Creative Commons Zero (CC0) license: https://creativecommons.org/publicdomain/zero/1.0/
+
+- https://pickupimage.com/free-photos/business-office-meeting/2352065
+- https://free-images.com/display/workplace_team_business_meeting.html
+- https://free-images.com/display/startup_meeting_brainstorming_594091.html
+- https://stocksnap.io/photo/woman-working-8LHTBAZW32
+- https://stocksnap.io/photo/woman-developers-FVWLQXWZ1P
+- https://stocksnap.io/photo/working-chromebook-GHIBZPY4FS
+- https://stocksnap.io/photo/laptop-code-0KAT80KW5F
+- https://stocksnap.io/photo/people-man-T9AY19HFC0
+- https://stocksnap.io/photo/business-woman-7U4SPRJEIE
+- https://pxhere.com/en/photo/912886
+- https://pxhere.com/en/photo/1543497
+- https://pxhere.com/en/photo/1448601
+
+== Changelog ==
+
+= 0.0.1 - 10 November 2022 =
+
+* Initial release
